1f11f451e3175cfc19079ac46dde7bdeda0388c1
KimNamHo2020 <147688974+KimNamHo2020@users.noreply.github.com>
2023-12-13 15:45:41 +0900

    Update Button.java

--- Ch9/Innerinterface/Button.java
+++ Ch9/Innerinterface/Button.java
@@ -1,5 +1,19 @@
-package innerinterface;
+package Innerinterface;
 
 public class Button {
-
+	OnClickListener listener;
+	
+	void SetOnClickListener(OnClickListener listener)
+	{
+		this.listener = listener;
+	}
+	
+	public void touch() 
+	{
+		listener.onClick();
+	}
+	
+	public interface OnClickListener{
+		void onClick();
+	}
 }
