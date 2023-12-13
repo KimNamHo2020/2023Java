46fc3a49d058e410779b005531906cdd4e9d1cdb
KimNamHo2020 <147688974+KimNamHo2020@users.noreply.github.com>
2023-12-13 16:25:09 +0900

    Update Main.java

--- src/ch7/Main.java
+++ src/ch7/Main.java
@@ -10,7 +10,7 @@ public class Main {
 //		KumhoTire kTire = new KumhoTire();
 //		sc.SetTire(kTire);
 //		sc.run();
-		//여기까지가 다형성 모를 때 쓰는 방법. 하위 타이어가 많아질수록 코드가 지저분해진다
+		
 		
 		Tire tire = new HankookTire();
 		NewCar sc = new NewCar(tire);
@@ -19,6 +19,5 @@ public class Main {
 		sc.SetTire(tire);
 		sc.run();
 		} 
-	//Car 와 NewCar 코드를 비교해보면 NewCar가 얼마나 간결한지 알 수 있다.
-
+	
 	}
