b1efaf661a4d4718243cfb3a29b75b3902186491
KimNamHo2020 <147688974+KimNamHo2020@users.noreply.github.com>
2023-12-13 15:47:59 +0900

    Update Main.java

--- Ch9/Test/Main.java
+++ Ch9/Test/Main.java
@@ -2,4 +2,21 @@ package Test;
 
 public class Main {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//A a = new A();
+
+		//System.out.println(a.memberA);
+		
+		//A.B b = new A.B(); 
+		
+		//A.C c = a.new C();
+		
+		Outter o = new Outter();
+		Outter.Nested n = o.new Nested();
+		n.method();
+		n.method2();
+		
+	}
+
 }
