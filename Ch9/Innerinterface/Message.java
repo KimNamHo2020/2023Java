25bbdc8e3008012c6b9654a56e9ba690f669d31d
KimNamHo2020 <147688974+KimNamHo2020@users.noreply.github.com>
2023-12-13 15:46:01 +0900

    Update Message.java

--- Ch9/Innerinterface/Message.java
+++ Ch9/Innerinterface/Message.java
@@ -2,4 +2,10 @@ package innerinterface;
 
 public class Message {
 
+}package Innerinterface;
+
+public class Message implements Button.OnClickListener {
+	public void onClick() {
+		System.out.println("Send Message");
+	}
 }
