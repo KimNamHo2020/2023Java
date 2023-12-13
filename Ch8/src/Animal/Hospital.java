310652e0a76566399ac576303799efe3ce8e2fbb
KimNamHo2020 <147688974+KimNamHo2020@users.noreply.github.com>
2023-12-13 21:44:58 +0900

    Update Hospital.java

--- Ch8/src/Animal/Hospital.java
+++ Ch8/src/Animal/Hospital.java
@@ -1,1 +1,14 @@
+package Animal;
 
+public class Hospital {
+//	Patient p = new Dog();
+	Patient p;
+	
+	void SetPatient(Patient p) {
+		this.p = p;
+	}
+	
+	void CheckSound() {
+		p.Sound();
+	}
+}
