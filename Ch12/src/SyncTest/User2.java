5e5966752e5df64a4758cae3efc0cb6111d0e902
KimNamHo2020 <147688974+KimNamHo2020@users.noreply.github.com>
2023-12-14 00:33:49 +0900

    Create User2.java

---
+++ Ch12/src/SyncTest/User2.java
@@ -0,0 +1,14 @@
+package SyncTest;
+
+public class User2 extends Thread {
+	private Calculator cal;
+	
+	public void setCalculator(Calculator cal) {
+		this.setName("User2"); 
+		this.cal = cal;
+	}
+	
+	public void run() {
+		cal.setMemory(50);
+	}
+}
