bbc0ec065e18070105e234694375b76417f19b3d
KimNamHo2020 <147688974+KimNamHo2020@users.noreply.github.com>
2023-12-13 16:11:13 +0900

    Update MyClass.java

--- Ch11/ObjectTest/MyClass.java
+++ Ch11/ObjectTest/MyClass.java
@@ -1,1 +1,23 @@
+package ObjectTest;
 
+public class MyClass {
+	public String toString() {
+		return "HI";
+	}
+	
+	public int id;
+	
+	public MyClass(int id) {
+		this.id = id;
+	}
+	
+	public boolean equals(Object obj) {
+		if(obj instanceof MyClass) {
+			MyClass myclass = (MyClass) obj;
+			if(myclass.id == id) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
