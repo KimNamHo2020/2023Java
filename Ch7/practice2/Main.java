4347f35854a6994dcb8685fdbb7ca6c92e55a122
KimNamHo2020 <147688974+KimNamHo2020@users.noreply.github.com>
2023-12-13 21:33:19 +0900

    Delete Ch7/practice2 directory

--- Ch7/practice2/Main.java
+++
@@ -1,9 +0,0 @@
-package practice2;
-
-public class Main {
-
-	public static void main(String[] args) {		
-		Weapon a = new Sword();
-		a.attack();
-	}
-}
--- Ch7/practice2/Weapon.java
+++
@@ -1,8 +0,0 @@
-package practice2;
-
-public class Weapon {
-	
-	public void attack() {
-		System.out.println("attack");
-	}
-}
