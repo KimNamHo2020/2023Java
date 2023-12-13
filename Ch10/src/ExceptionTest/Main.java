cbfa1124c1ce256a7ea463b34e18bb7007d08b65
KimNamHo2020 <147688974+KimNamHo2020@users.noreply.github.com>
2023-12-13 15:59:26 +0900

    Update Main.java

--- Ch10/src/ExceptionTest/Main.java
+++ Ch10/src/ExceptionTest/Main.java
@@ -2,4 +2,34 @@ package ExceptionTest;
 
 public class Main {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DivideZero d = new DivideZero();
+		
+//		try {
+//			d.method();
+//		}
+//		catch(NullPointerException e) { 
+//			System.out.println(e);
+//		}
+//		
+//		catch(ArithmeticException e) {
+//			System.out.println(e);
+//		}
+//		
+//		finally { 
+//			System.out.println("this block always run");
+//		}
+//		
+//		System.out.println("I'm alive.");
+//		//이 try-catch .
+		
+		try {
+			Class c = Class.forName("java.lang.String");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+	}
 }
