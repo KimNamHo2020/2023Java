f0a139d5dbbbd4df1d6f80cc8265b54a37635de9
KimNamHo2020 <147688974+KimNamHo2020@users.noreply.github.com>
2023-12-13 16:18:56 +0900

    Update User1.java

--- Ch12/SyncTest/User1.java
+++ Ch12/SyncTest/User1.java
@@ -4,7 +4,7 @@ public class User1 extends Thread {
 	private Calculator cal;
 	
 	public void setCalculator(Calculator cal) {
-		this.setName("User1"); //Thread에서 온거
+		this.setName("User1"); 
 		this.cal = cal;
 	}
 	
